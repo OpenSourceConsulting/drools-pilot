4464990c0cdca3e6085172644ca344a0ddcc4224
idkbj <idkbj@osci.kr>
2015-11-20 18:39:09 +0900

    Junit CEPDataConsumerTest 추가.

---
+++ pilot-fris/src/test/java/com/nicecredit/pilot/consumer/MockCEPDataConsumer.java
@@ -0,0 +1,38 @@
+package com.nicecredit.pilot.consumer;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Envelope;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ * @author BongJin Kwon
+ */
+public class MockCEPDataConsumer extends CEPDataConsumer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MockCEPDataConsumer.class);
+	/**
+	 * <pre>
+	 * 
+	 * </pre>
+	 * @param channel
+	 */
+	public MockCEPDataConsumer(Channel channel) {
+		super(channel);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void sendAck(Envelope envelope) throws IOException {
+		LOGGER.debug("send ack.");
+	}
+	
+
+}
+//end of MockCEPDataConsumer.java
